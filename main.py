47535063481cc883763eb12a3903f492fc8e40b9
FAbrickA <62110042+FAbrickA@users.noreply.github.com>
2021-01-10 20:58:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -649,8 +649,8 @@ def pin_message(peer_id, conversation_message_id):
                               'conversation_message_id': get_all_minimal_message(peer_id)})
 
 
-send_poll = SendTodayPoll(hours=20, minutes=20)
-finish_poll = FinishTodayPoll(hours=8)
+send_poll = SendTodayPoll(hours=18)
+finish_poll = FinishTodayPoll(hours=8, minutes=10)
 send_poll.start()
 finish_poll.start()
 
